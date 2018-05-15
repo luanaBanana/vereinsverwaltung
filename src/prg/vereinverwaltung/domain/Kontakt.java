9f95be2fcf645b201fdfc839a5662eb62b7b1d64
bdigg <bdigg@SHIT-MOBILE>
2018-05-15 17:50:03 +0200

    helooooo

--- src/prg/vereinverwaltung/domain/Kontakt.java
+++ src/prg/vereinverwaltung/domain/Kontakt.java
@@ -1,12 +1,5 @@
 package prg.vereinverwaltung.domain;
 
-/**
- * Diese Klasse bildet die Kontaktdaten ab.
- * 
- * @author jsucur
- * @version 1.0
- */
-
 public class Kontakt {
 
 	private int telefon;
