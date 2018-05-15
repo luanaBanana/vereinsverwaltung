adcac25204a439212e3f54f2e948d707ab4bfa77
bdigg <bdigg@SHIT-MOBILE>
2018-05-15 17:29:35 +0200

    style class kontakt

--- src/prg/vereinverwaltung/domain/Kontakt.java
+++ src/prg/vereinverwaltung/domain/Kontakt.java
@@ -11,21 +11,21 @@ public class Kontakt {
 
 	private int telefon;
 	private String email;
-	
-	
+
 	public int getTelefon() {
 		return telefon;
 	}
+
 	public void setTelefon(int telefon) {
 		this.telefon = telefon;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
 
 }
