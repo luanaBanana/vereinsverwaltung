a7ba265066e5d7eb2d2361ca1f6b26e1e56db26f
valen <valen@10.155.101.23>
2018-05-15 17:24:05 +0200

    Hello Team!

--- src/prg/vereinverwaltung/domain/Adresse.java
+++ src/prg/vereinverwaltung/domain/Adresse.java
@@ -9,6 +9,32 @@ package prg.vereinverwaltung.domain;
  */
 public class Adresse {
 
-	// TODO ...
+	private int plz;
+	private String strasse;
+	private String ort;
+
+	public int getPlz() {
+		return plz;
+	}
+
+	public void setPlz(int plz) {
+		this.plz = plz;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
 
 }
