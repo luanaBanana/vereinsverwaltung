3dbea43f9167d5cab1a4cff0256fd719209b0cb1
bdigg <bdigg@SHIT-MOBILE>
2018-05-15 17:27:54 +0200

    style class person

--- src/prg/vereinverwaltung/domain/Person.java
+++ src/prg/vereinverwaltung/domain/Person.java
@@ -10,28 +10,32 @@ import java.time.LocalDate;
  */
 public class Person {
 
-		private String name;
-		private String vorname;
-		private LocalDate geburtsdatum;
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getVorname() {
-			return vorname;
-		}
-		public void setVorname(String vorname) {
-			this.vorname = vorname;
-		}
-		public LocalDate getGeburtsdatum() {
-			return geburtsdatum;
-		}
-		public void setGeburtsdatum(LocalDate geburtsdatum) {
-			this.geburtsdatum = geburtsdatum;
-		}
-		
+	private String name;
+	private String vorname;
+	private LocalDate geburtsdatum;
+
+	public String getName() {
+		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public LocalDate getGeburtsdatum() {
+		return geburtsdatum;
+	}
+
+	public void setGeburtsdatum(LocalDate geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
+	}
+
+}
